b90b07c77b61cc033ad3df35c8cb4216bff93e6c
Bogdan Lameko <bogdan.lmk@gmail.com>
2025-03-26 15:11:47 +0200

    rewrite

--- src/reporting/llm_reporter.py
+++ src/reporting/llm_reporter.py
@@ -19,7 +19,7 @@ class LLMReporter(BaseReporter):
         super().__init__(input_data_path=input_data_path)
         self.model = model
         self.max_tokens = max_tokens
-        openai.api_key = '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'
+        openai.api_key = OPENAI_API_KEY
         if not openai.api_key:
             raise ValueError("OPENAI_API_KEY environment variable is required")
 
